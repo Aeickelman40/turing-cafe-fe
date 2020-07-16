870c93907b2f99a91b1b98de4b9480617c6bb3ad
Alex Eickelman <aeickelman40@gmail.com>
2020-07-16 11:48:30 -0600

    Add App integration test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render,fireEvent} from "@testing-library/react";
+import "@testing-library/jest-dom/";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe("App", () => {
+  it("should let user add a new reservation on Make Reservation click", () => {
+    // Setup: render the app
+    const { getByText, getByPlaceholderText } = render(<App />);
+
+    // Execution: grab elements we want to find on the dom, fire events for changing inputs and submit button click
+    const nameInput = getByPlaceholderText('Name');
+    const dateInput = getByPlaceholderText('Date (mm/dd)');
+    const timeInput = getByPlaceholderText('Time');
+    const guestInput = getByPlaceholderText('Number of guests')
+    const submitReservationBtn = getByText('Make Reservation')
+    fireEvent.change(nameInput, { target: { value: "Alex" }});
+    fireEvent.change(dateInput, { target: { value: "7/17" }});
+    fireEvent.change(timeInput, {target: { value: "6:30" }});
+    fireEvent.change(guestInput, {target: { value: 8 }});
+    fireEvent.click(submitReservationBtn);
+
+    // Assertion: Check to make sure newly submitted reservation is appearing on the dom
+    expect(getByText("Alex")).toBeInTheDocument();
+    expect(getByText("7/17")).toBeInTheDocument();
+    expect(getByText("6:30 pm")).toBeInTheDocument();
+    expect(getByText("Number of guests: 8")).toBeInTheDocument();
+  });
+});
+
--- src/ReservationCard/ReservationCard.test.js
+++ src/ReservationCard/ReservationCard.test.js
@@ -4,15 +4,21 @@ import "@testing-library/jest-dom";
 import ReservationCard from "./ReservationCard";
 
 describe("ReservationCard", () => {
-    
+
   it("should render the reservation data", () => {
+    // Setup: render the card component
     const { getByText } = render(
       <ReservationCard name={"Alex"} date={"7/16"} time={"8:30"} number={5} />
     );
+
+    // Execution: grab the elements we want to have showing up
+    
     const name = getByText("Alex");
     const date = getByText("7/16");
     const time = getByText("8:30 pm");
     const guests = getByText("Number of guests: 5");
+    // Assertion: make sure those elements are showing up
+
     expect(name).toBeInTheDocument();
     expect(date).toBeInTheDocument();
     expect(time).toBeInTheDocument();
