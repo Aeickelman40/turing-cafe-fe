54294dee0b62e0a8b222a39d145512bdb3183c60
Alex Eickelman <aeickelman40@gmail.com>
2020-07-16 09:41:21 -0600

    Add Reservations file along with componentDidMount within App for fetch request

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import { getReservations } from '../apiCalls';
 
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      
+    }
+  }
+
+
+  componentDidMount = async () => {
+    try {
+      const reservations = await getReservations();
+      this.setState({ reservation: reservations})
+    } catch(error) {
+      this.setState({ error: error })
+    }
+
+  }
+
+
   render() {
+    console.log(this.state)
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
